c4439084fd6a138776289214296a1f817deb3f6a
NBJena <135602685+addgit02@users.noreply.github.com>
2023-06-06 12:37:06 +0530

    Add files via upload

---
+++ GetDetailsOne/src/test/java/Test/NewTestMemberSearch.java
@@ -0,0 +1,29 @@
+package Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class NewTestMemberSearch {
+	WebDriver driver;
+
+	@Test
+	public void findMember() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", ".\\src\\main\\resources\\chromedriver.exe");
+		ChromeOptions ops = new ChromeOptions();
+		ops.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(ops);
+		Thread.sleep(1000);
+		driver.manage().window().maximize();
+		driver.get("https://google.com");
+		System.out.println("Hello & Hi !!!!");
+		Thread.sleep(5000);
+		driver.close();
+			}
+}
